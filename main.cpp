c0b835281579ccf298137be3bfd7d2fa18dbd59c
Ben Margret <162319953+looydmence@users.noreply.github.com>
2024-03-05 14:16:55 +0200

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,17 @@
+ if(!(manager()->load(plugin) & PluginManager::LoadState::Loaded)) {
+        Error{} << "Trade::AnyImageImporter::openFile(): cannot load the" << plugin << "plugin";
+        return;
+    }
+
+    const PluginManager::PluginMetadata* const metadata = manager()->metadata(plugin);
+    CORRADE_INTERNAL_ASSERT(metadata);
+    if(flags() & ImporterFlag::Verbose) {
+        Debug d;
+        d << "Trade::AnyImageImporter::openFile(): using" << plugin;
+        if(plugin != metadata->name())
+            d << "(provided by" << metadata->name() << Debug::nospace << ")";
+    }
+
+  Containers::Pointer<AbstractImporter> importer = static_cast<PluginManager::Manager<AbstractImporter>*>(manager())->instantiate(plugin);
+    importer->setFlags(flags());
+    if(fileCallback()) importer->setFileCallback(fileCallback(), fileCallbackUserData());
